446643f8e91ae5600e49b11f24beef58b61ceb5b
Kaitlyn <kaitlyn.shae28@gmail.com>
2023-11-28 17:34:37 -0500

    y

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-/* Your Code Here */
-
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -21,3 +19,62 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(employeeData){
+    return {
+        firstName: employeeData[0],
+        familyName: employeeData[1],
+        title: employeeData[2],
+        payPerHour: employeeData[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+
+function createEmployeeRecords(nestedArray){
+    return nestedArray.map(employeeArray => createEmployeeRecord(employeeArray));
+}
+
+function createTimeInEvent(dateStamp) {
+    const [date, time] = dateStamp.split(" ");
+    const [hourString] = time.split(":");
+    const hour = parseInt(hourString);
+    const timeInEvent = {
+      type: "TimeIn",
+      hour: hour,
+      date: date,
+    };
+    this.timeInEvents.push(timeInEvent);
+    return this;
+  }
+
+  function createTimeOutEvent(dateStamp) {
+    const [date, time] = dateStamp.split(" ");
+    const [hourString] = time.split(":");
+    const hour = parseInt(hourString);
+    const timeOutEvent = {
+      type: "TimeOut",
+      hour: hour,
+      date: date,
+    };
+    this.timeOutEvents.push(timeOutEvent);
+    return this;
+  }
+
+  function hoursWorkedOnDate(date){
+    const timeInEvent = this.timeInEvents.find(event => event.date === date);
+    const timeOutEvent = this.timeOutEvents.find(event => event.date === date);
+  
+    const timeInHour = timeInEvent.hour;
+    const timeOutHour = timeOutEvent.hour;
+  
+    const hoursWorked = timeOutHour - timeInHour;
+  
+    return hoursWorked/100
+  }
+
+  function wagesEarnedOnDate(employeeRecord, date){
+    const hoursWorked = hoursWorkedOnDate(employeeRecord, date);
+    const payRate = employeeRecord.payPerHour;
+    const wagesEarned = hoursWorked * payRate;
+    return wagesEarned;
+  }
